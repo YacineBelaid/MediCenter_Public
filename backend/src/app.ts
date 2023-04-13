05c72683e9fa96da5edc837609be23f55833b0ab
Hadji, Nadir <hadji.nadir@courrier.uqam.ca>
2023-04-13 02:41:46 -0400

    fix build

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -8,7 +8,7 @@ import hpp from 'hpp';
 import morgan from 'morgan';
 import swaggerJSDoc from 'swagger-jsdoc';
 import swaggerUi from 'swagger-ui-express';
-import { NODE_ENV, PORT, LOG_FORMAT, ORIGIN, CREDENTIALS } from '@config';
+import { NODE_ENV, PORT, LOG_FORMAT, ORIGIN } from '@config';
 import { Routes } from '@interfaces/routes.interface';
 import { ErrorMiddleware } from '@middlewares/error.middleware';
 import { logger, stream } from '@utils/logger';
@@ -44,7 +44,7 @@ export class App {
 
   private initializeMiddlewares() {
     this.app.use(morgan(LOG_FORMAT, { stream }));
-    this.app.use(cors({ origin: ORIGIN, credentials: CREDENTIALS }));
+    this.app.use(cors({ origin: ORIGIN }));
     this.app.use(hpp());
     this.app.use(helmet());
     this.app.use(compression());
--- backend/src/config/index.ts
+++ backend/src/config/index.ts
@@ -1,5 +1,6 @@
 import { config } from 'dotenv';
-config({ path: `.env.${process.env.NODE_ENV || 'development'}.local` });
+console.log(`.env.${process.env.NODE_ENV || 'development'}`);
 
-export const CREDENTIALS = process.env.CREDENTIALS === 'true';
+config({ path: `.env.${process.env.NODE_ENV || 'development'}` });
+
 export const { NODE_ENV, PORT, SECRET_KEY, LOG_FORMAT, LOG_DIR, ORIGIN } = process.env;
